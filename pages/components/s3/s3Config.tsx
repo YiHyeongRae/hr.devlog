325dec56c33a80557a7b1f2b8b54fd7dcbefc966
yihyeongrae <dev.yihr@luxurynall.com>
2022-12-07 16:47:52 +0900

    feat: [vercel 배포 오류 수정] react-aws-s3-typescript 삭제

--- pages/components/s3/s3Config.tsx
+++
@@ -1,9 +0,0 @@
-export const s3Config = {
-  bucketName: "hr.devlog",
-  dirName: "test-directory" /* Optional */,
-  region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  s3Url:
-    "http://hr.devlog.s3-website.ap-northeast-2.amazonaws.com/" /* Optional */,
-};
