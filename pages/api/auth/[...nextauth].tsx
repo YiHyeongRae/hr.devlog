d4005992b524425946d95ce94ad0fb405faace87
YiHyeongRae <dev.yihr@gmail.com>
2022-12-13 16:56:28 +0900

    feat: vercel 배포 오류 - import 경로 수정

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import Hash from "../../components/Hash/Hash";
+import Hash from "../../components/Atoms/Hash";
 
 interface userResData {
   user: {
