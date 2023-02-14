d4b382352a1a0ed528e409ad2b7a8f103bd5f1de
YiHyeongRae <dev.yihr@gmail.com>
2023-02-14 14:43:35 +0900

    feat: webpack 번들 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const removeImports = require("next-remove-imports")();
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
@@ -13,6 +17,22 @@ const nextConfig = {
   },
 };
 
-module.exports = removeImports({
+(module.exports = removeImports({
   ...nextConfig,
-});
+})),
+  withBundleAnalyzer({
+    compress: true,
+    webpack(config, { webpack }) {
+      const prod = process.env.NODE_ENV === "production";
+      const plugins = [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      ];
+      return {
+        ...config,
+        mode: prod ? "production" : "development",
+        devtool: prod ? "hidden-source-map" : "eval",
+        plugins,
+      };
+    },
+  });
