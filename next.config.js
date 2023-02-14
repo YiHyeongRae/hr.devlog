ba4b58e2072fe341b28a1f6a057c4ff433e7f6c6
YiHyeongRae <dev.yihr@gmail.com>
2023-02-14 13:10:14 +0900

    feat: font 파일명 변경

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const removeImports = require('next-remove-imports')();
+const removeImports = require("next-remove-imports")();
 const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
-
-}
+  compiler: {
+    styledComponents: true,
+  },
+};
 
 module.exports = removeImports({
-  ...nextConfig
-})
+  ...nextConfig,
+});
---
+++ pages/pagenation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Paging = styled.div`
+  width: 100%;
+  height: 200vh;
+`;
+function pagenation() {
+  return <div>pagenation</div>;
+}
+
+export default pagenation;
