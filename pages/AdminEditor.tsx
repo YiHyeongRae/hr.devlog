09a40af471c602426ab185986c644d9582058a30
YiHyeongRae <dev.yihr@gmail.com>
2023-02-14 15:30:43 +0900

    feat: 최적화 수정

--- pages/AdminEditor.tsx
+++ pages/AdminEditor.tsx
@@ -5,60 +5,9 @@ import { useS3Upload } from "next-s3-upload";
 import axios from "axios";
 import Editor from "../components/MDEditor/MDEditor";
 import { ICommand } from "@uiw/react-md-editor/lib/commands";
-import Image from "next/image";
+
 import styled from "styled-components";
-import AutoHeightImage from "../components/AutoHeightImage";
 
-const CircleWrap = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-content: center;
-  list-style: none;
-  margin: 50px 0;
-`;
-
-const CircleList = styled.li`
-  width: 500px;
-
-  box-sizing: border-box;
-  box-shadow: 0 0 10px #333;
-  cursor: pointer;
-  border-radius: 10px;
-`;
-
-const ThunmbNail = styled.div`
-  position: relative;
-  box-sizing: border-box;
-  width: 500px;
-  height: 300px;
-  border-radius: 10px 10px 0 0;
-
-  border: 1px solid #999;
-  // animation: fadeInOut 5s linear infinite;
-`;
-
-const ThunmbNailTitle = styled.div`
-  text-align: center;
-  width: 100%;
-
-  font-size: 16px;
-`;
-
-const TagWrap = styled.ul`
-  margin-top: 25px;
-`;
-
-const PostTag = styled.li`
-  display: inline-block;
-  font-size: 10px;
-  font-family: "MapleLight";
-  padding: 10px;
-  background-color: #333;
-  border-radius: 10px;
-  margin-right: 10px;
-  margin-bottom: 10px;
-`;
 const PostTitle = styled.div`
   display: flex;
 
@@ -92,12 +41,6 @@ const ImgTitle = styled.div`
   font-family: "MapleLight";
 `;
 
-const ContentWrap = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-`;
-
 const TapWrap = styled.ul`
   background-color: #2c2d2d;
   display: flex;
@@ -115,22 +58,22 @@ const TapTitle = styled.li`
   padding-left: 16px;
 `;
 const AdminEditor: NextPage = () => {
-  const title3: ICommand = {
-    name: "title3",
-    keyCommand: "title3",
-    buttonProps: { "aria-label": "Insert title3" },
-    icon: (
-      <svg width="12" height="12" viewBox="0 0 520 520">
-        <path
-          fill="currentColor"
-          d="M15.7083333,468 C7.03242448,468 0,462.030833 0,454.666667 L0,421.333333 C0,413.969167 7.03242448,408 15.7083333,408 L361.291667,408 C369.967576,408 377,413.969167 377,421.333333 L377,454.666667 C377,462.030833 369.967576,468 361.291667,468 L15.7083333,468 Z M21.6666667,366 C9.69989583,366 0,359.831861 0,352.222222 L0,317.777778 C0,310.168139 9.69989583,304 21.6666667,304 L498.333333,304 C510.300104,304 520,310.168139 520,317.777778 L520,352.222222 C520,359.831861 510.300104,366 498.333333,366 L21.6666667,366 Z M136.835938,64 L136.835937,126 L107.25,126 L107.25,251 L40.75,251 L40.75,126 L-5.68434189e-14,126 L-5.68434189e-14,64 L136.835938,64 Z M212,64 L212,251 L161.648438,251 L161.648438,64 L212,64 Z M378,64 L378,126 L343.25,126 L343.25,251 L281.75,251 L281.75,126 L238,126 L238,64 L378,64 Z M449.047619,189.550781 L520,189.550781 L520,251 L405,251 L405,64 L449.047619,64 L449.047619,189.550781 Z"
-        />
-      </svg>
-    ),
-    execute: (state: any, api: any) => {
-      console.log(1);
-    },
-  };
+  // const title3: ICommand = {
+  //   name: "title3",
+  //   keyCommand: "title3",
+  //   buttonProps: { "aria-label": "Insert title3" },
+  //   icon: (
+  //     <svg width="12" height="12" viewBox="0 0 520 520">
+  //       <path
+  //         fill="currentColor"
+  //         d="M15.7083333,468 C7.03242448,468 0,462.030833 0,454.666667 L0,421.333333 C0,413.969167 7.03242448,408 15.7083333,408 L361.291667,408 C369.967576,408 377,413.969167 377,421.333333 L377,454.666667 C377,462.030833 369.967576,468 361.291667,468 L15.7083333,468 Z M21.6666667,366 C9.69989583,366 0,359.831861 0,352.222222 L0,317.777778 C0,310.168139 9.69989583,304 21.6666667,304 L498.333333,304 C510.300104,304 520,310.168139 520,317.777778 L520,352.222222 C520,359.831861 510.300104,366 498.333333,366 L21.6666667,366 Z M136.835938,64 L136.835937,126 L107.25,126 L107.25,251 L40.75,251 L40.75,126 L-5.68434189e-14,126 L-5.68434189e-14,64 L136.835938,64 Z M212,64 L212,251 L161.648438,251 L161.648438,64 L212,64 Z M378,64 L378,126 L343.25,126 L343.25,251 L281.75,251 L281.75,126 L238,126 L238,64 L378,64 Z M449.047619,189.550781 L520,189.550781 L520,251 L405,251 L405,64 L449.047619,64 L449.047619,189.550781 Z"
+  //       />
+  //     </svg>
+  //   ),
+  //   execute: (state: any, api: any) => {
+  //     console.log(1);
+  //   },
+  // };
   const [S3File, setS3File] = useState<any>();
   // const setPostDesc: Function = (e: any) => {
   //   console.log("?z", e);
