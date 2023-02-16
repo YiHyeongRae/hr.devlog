0f290b9622b2034549ef25eb015501a43d67c381
YiHyeongRae <dev.yihr@gmail.com>
2023-02-16 21:37:29 +0900

    feat: 최적화를 위한 analyzer 설치

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import { useRouter } from "next/router";
 import { Suspense, useEffect, useState } from "react";
--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const removeImports = require("next-remove-imports")();
+const withPlugins = require("next-compose-plugins");
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig = {
   reactStrictMode: false,
@@ -14,6 +19,48 @@ const nextConfig = {
   },
 };
 
-module.exports = removeImports({
-  ...nextConfig,
-});
+module.exports = withPlugins([
+  [removeImports({ ...nextConfig })],
+  [
+    withBundleAnalyzer({
+      compress: true,
+      webpack(nextConfig, { webpack }) {
+        const prod = process.env.NODE_ENV === "production";
+        const plugins = [
+          ...nextConfig.plugins,
+          new webpack.ContextReplacementPlugin(
+            /moment[/\\]locale$/,
+            /^\.\/ko$/
+          ),
+        ];
+        return {
+          ...nextConfig,
+          mode: prod ? "production" : "development",
+          devtool: prod ? "hidden-source-map" : "eval",
+          plugins,
+        };
+      },
+    }),
+  ],
+
+  // your other plugins here
+]);
+// (module.exports = removeImports({
+//   ...nextConfig,
+// })),
+//   withBundleAnalyzer({
+//     compress: true,
+//     webpack(nextConfig, { webpack }) {
+//       const prod = process.env.NODE_ENV === "production";
+//       const plugins = [
+//         ...nextConfig.plugins,
+//         new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+//       ];
+//       return {
+//         ...nextConfig,
+//         mode: prod ? "production" : "development",
+//         devtool: prod ? "hidden-source-map" : "eval",
+//         plugins,
+//       };
+//     },
+//   });
