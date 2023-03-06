07826fc16c7d24b6250b03abdff4621b66303376
YiHyeongRae <dev.yihr@gmail.com>
2023-03-07 08:59:40 +0900

    feat: head meta 추가

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -10,13 +10,21 @@ const SEO = ({ title }: SEOTYPES) => {
     <>
       <Head>
         <title>{`${title} - hr.devlog`}</title>
-
+        <meta name="theme-color" content="#000000" />
         <meta charSet="utf-8" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=0"
         />
-        <meta name="description" content="hr-devlog" />
+        <meta name="title" content="hr-devlog" />
+        <meta name="description" content="개발 기록을 남기는 개인 블로그" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="hr-devlog" />
+        <meta
+          property="og:description"
+          content="개발 기록을 남기는 개인 블로그"
+        />{" "}
+        <meta property="og:url" content="https://hr-devlog.vercel.app/" />
       </Head>
     </>
   );
