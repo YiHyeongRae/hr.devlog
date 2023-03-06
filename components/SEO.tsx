106f9be28436cab3377eb41ebe37b7838f6fe4b6
YiHyeongRae <dev.yihr@gmail.com>
2023-03-06 13:21:43 +0900

    feat: google 검색 엔진 등록

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -9,6 +9,10 @@ const SEO = ({ title }: SEOTYPES) => {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="M4BHCocjqAs-zP9KpT60AvVZ4sDqXu9De_tgOwk5Ozg"
+        />
         <meta charSet="utf-8" />
 
         <meta
