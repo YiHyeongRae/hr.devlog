53830ca44fbaf32ab1f4ea9904991c17952c97c1
YiHyeongRae <dev.yihr@gmail.com>
2023-03-06 13:40:04 +0900

    feat: GSC html 추가

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -10,10 +10,7 @@ const SEO = ({ title }: SEOTYPES) => {
     <>
       <Head>
         <title>{`${title} - hr.devlog`}</title>
-        <meta
-          name="google-site-verification"
-          content="M4BHCocjqAs-zP9KpT60AvVZ4sDqXu9De_tgOwk5Ozg"
-        />
+
         <meta charSet="utf-8" />
         <meta
           name="viewport"
