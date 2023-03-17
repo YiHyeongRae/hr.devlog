9f922247e3b711b9c8686fbb02d52bd446ceb679
YiHyeongRae <dev.yihr@gmail.com>
2023-03-17 18:23:55 +0900

    feat: 주석 설명 작성

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,19 +14,29 @@ import Script from "next/script";
 import * as gtag from "../lib/gtag";
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  // ssr 환경 redux를 위한 wrapper 작업
   const { store } = wrapper.useWrappedStore(pageProps);
 
+  // 페이지 이동간 카운팅 해줄 state
   const [timer, setTimer] = useState(3);
+
+  // Auth 체크를 위한 func
   function Auth({ children }: any) {
+    // useSeesion으로 login 상태 체크
     const { data: session, status } = useSession();
     const isUser = !!session?.user;
+
     useEffect(() => {
+      // status 로 상태 체크
       if (status === "loading") return; // Do nothing while loading
+
+      // 로그인 되있지 않다면 setInterval 과 함께 안내페이지 및 카운팅
       if (!isUser) {
         const dec = setInterval(() => {
           setTimer(timer - 1);
         }, 1000);
 
+        // 카운팅 끝날 시 강제 페이지이동
         if (timer === 0) {
           clearInterval(dec);
           signIn();
@@ -35,6 +45,7 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       // setTimeout(() => signIn(), 5000); // If not authenticated, force log in
     }, [isUser, status]);
 
+    // 로그인 되있다면 정상적으로 page 호출
     if (isUser) {
       return children;
     }
@@ -72,12 +83,16 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     );
   }
 
+  // router 및 routing 간 로딩스피너로 사용할 state
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   useEffect(() => {
+    // GA 이벤트 등록
     const handleRouteChange = (url: string) => {
       gtag.pageview(url);
     };
+
+    // router.events 로 라우팅 이벤트 캡쳐링 및 상태에 맞게 함수 실행
     router.events.on("routeChangeStart", () => setLoading(true));
     router.events.on("routeChangeComplete", () => setLoading(false)),
       handleRouteChange;
@@ -88,6 +103,8 @@ function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         handleRouteChange;
       router.events.off("routeChangeError", () => setLoading(false));
     };
+
+    // 디펜던시로 router.events 등록
   }, [router.events]);
   return (
     <SessionProvider session={session}>
