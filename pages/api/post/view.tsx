2efadc7ab115086cfbcfa576747cc01f33709316
YiHyeongRae <dev.yihr@gmail.com>
2023-04-05 16:29:26 +0900

    feat: 조회수 증가 api 추가

---
+++ pages/api/post/view.tsx
@@ -0,0 +1,34 @@
+// import type { NextApiRequest, NextApiResponse } from "next";
+// const db = require("../../common/config/db");
+
+// export default function postReg(req: NextApiRequest, res: NextApiResponse) {
+//   db.query(
+//     "INSERT INTO blog_post (
+//     function (err: any, data: any) {
+//       if (!err) {
+//         res.send(data);
+//       } else {
+//         console.log(err);
+//         res.send(err);
+//       }
+//     }
+//   );
+// }
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const db = require("../../../common/config/db");
+
+export default function postReg(req: NextApiRequest, res: NextApiResponse) {
+  console.log("view console =====", req);
+  db.query(
+    `UPDATE blog_post SET view = view+1 WHERE no = ${req.body.data.postNo}`,
+    function (err: any, result: any) {
+      if (err) {
+        console.log("인서트 에러", err);
+      } else {
+        console.log("인서트 성공 ?", result);
+        // res.json("인서트 제이슨", result);
+      }
+    }
+  );
+}
