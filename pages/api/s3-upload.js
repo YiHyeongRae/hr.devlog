e60e2bc4606ab851b56da7df2af08cc7988d6391
YiHyeongRae <dev.yihr@gmail.com>
2023-04-07 10:37:09 +0900

    fix: s3 업로드 path 수정

--- pages/api/s3-upload.js
+++ pages/api/s3-upload.js
@@ -4,12 +4,12 @@ export default APIRoute.configure({
   key(req) {
     // console.log("분기 태워야함", req.body.type);
 
-    return `post/${req.body.no}/req.body.fileName`;
-    // if(req.body.type === "image"){
-    //   return `hr-devlog-image/${req.body.fileName}`;
-    // }else{
-    //   return `hr-devlog-text/${req.body.fileName}`;
-    // }
+    // return `post/${req.body.no}/${req.body.fileName}`;
+    if (req.body.type === "image") {
+      return `hr-devlog-image/${req.body.fileName}`;
+    } else {
+      return `hr-devlog-text/${req.body.fileName}`;
+    }
     // console.log("파일네임 어디서 가져옴 ?",req.body)
   },
 });
